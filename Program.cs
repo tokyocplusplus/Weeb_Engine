d36f8321b19b272699a893a6d65fcf461d1920c4
- [zenith] - <tokyocplusplus@gmail.com>
2025-05-23 14:31:18 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,51 +23,58 @@ namespace mainFile
 
         private int ebo;
         private float spinnyvalue = 1.0f;
+        private float spinnybutx = 1.0f;
         //commenting out the ebo shit so then it can be used for indices later idk
         public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title }) { }
         float[] vertices = {
-            -0.5f, -0.5f, -0.5f,  1.0f,0.0f,1.0f,
-            0.5f, -0.5f, -0.5f,  1.0f,0.0f,1.0f,
-            0.5f,  0.5f, -0.5f,  1.0f,0.0f,1.0f,
-            0.5f,  0.5f, -0.5f,  1.0f,0.0f,1.0f,
-            -0.5f,  0.5f, -0.5f,  1.0f,0.0f,1.0f,
-            -0.5f, -0.5f, -0.5f, 1.0f,0.0f,1.0f,
+            // Back face - Red
+            -0.5f, -0.5f, -0.5f,  1f, 0f, 0f, // Red
+            0.5f, -0.5f, -0.5f,  1f, 0f, 0f, 
+            0.5f,  0.5f, -0.5f,  1f, 0f, 0f, 
+            0.5f,  0.5f, -0.5f,  1f, 0f, 0f, 
+            -0.5f,  0.5f, -0.5f,  1f, 0f, 0f, 
+            -0.5f, -0.5f, -0.5f,  1f, 0f, 0f, 
 
-            -0.5f, -0.5f,  0.5f,  1.0f,0.0f,1.0f,
-            0.5f, -0.5f,  0.5f,  1.0f,0.0f,1.0f,
-            0.5f,  0.5f,  0.5f,  1.0f,0.0f,1.0f,
-            0.5f,  0.5f,  0.5f,  1.0f,0.0f,1.0f,
-            -0.5f,  0.5f,  0.5f,  1.0f,0.0f,1.0f,
-            -0.5f, -0.5f,  0.5f,  1.0f,0.0f,1.0f,
+            // Front face - Orange
+            -0.5f, -0.5f,  0.5f,  1f, 0.5f, 0f, // Orange
+            0.5f, -0.5f,  0.5f,  1f, 0.5f, 0f, 
+            0.5f,  0.5f,  0.5f,  1f, 0.5f, 0f, 
+            0.5f,  0.5f,  0.5f,  1f, 0.5f, 0f, 
+            -0.5f,  0.5f,  0.5f,  1f, 0.5f, 0f, 
+            -0.5f, -0.5f,  0.5f,  1f, 0.5f, 0f, 
 
-            -0.5f,  0.5f,  0.5f,  1.0f,0.0f,1.0f,
-            -0.5f,  0.5f, -0.5f,  1.0f,0.0f,1.0f,
-            -0.5f, -0.5f, -0.5f, 1.0f,0.0f,1.0f,
-            -0.5f, -0.5f, -0.5f, 1.0f,0.0f,1.0f,
-            -0.5f, -0.5f,  0.5f,  1.0f,0.0f,1.0f,
-            -0.5f,  0.5f,  0.5f,  1.0f,0.0f,1.0f,
+            // Left face - Yellow
+            -0.5f,  0.5f,  0.5f,  1f, 1f, 0f, // Yellow
+            -0.5f,  0.5f, -0.5f,  1f, 1f, 0f, 
+            -0.5f, -0.5f, -0.5f,  1f, 1f, 0f, 
+            -0.5f, -0.5f, -0.5f,  1f, 1f, 0f, 
+            -0.5f, -0.5f,  0.5f,  1f, 1f, 0f, 
+            -0.5f,  0.5f,  0.5f,  1f, 1f, 0f, 
 
-            0.5f,  0.5f,  0.5f, 1.0f,0.0f,1.0f,
-            0.5f,  0.5f, -0.5f, 1.0f,0.0f,1.0f,
-            0.5f, -0.5f, -0.5f,  1.0f,0.0f,1.0f,
-            0.5f, -0.5f, -0.5f,  1.0f,0.0f,1.0f,
-            0.5f, -0.5f,  0.5f, 1.0f,0.0f,1.0f,
-            0.5f,  0.5f,  0.5f, 1.0f,0.0f,1.0f,
+            // Right face - Green
+            0.5f,  0.5f,  0.5f,  0f, 1f, 0f, // Green
+            0.5f,  0.5f, -0.5f,  0f, 1f, 0f, 
+            0.5f, -0.5f, -0.5f,  0f, 1f, 0f, 
+            0.5f, -0.5f, -0.5f,  0f, 1f, 0f, 
+            0.5f, -0.5f,  0.5f,  0f, 1f, 0f, 
+            0.5f,  0.5f,  0.5f,  0f, 1f, 0f, 
 
-            -0.5f, -0.5f, -0.5f, 1.0f,0.0f,1.0f,
-            0.5f, -0.5f, -0.5f, 1.0f,0.0f,1.0f,
-            0.5f, -0.5f,  0.5f,  1.0f,0.0f,1.0f,
-            0.5f, -0.5f,  0.5f, 1.0f,0.0f,1.0f,
-            -0.5f, -0.5f,  0.5f,  1.0f,0.0f,1.0f,
-            -0.5f, -0.5f, -0.5f,  1.0f,0.0f,1.0f,
+            // Bottom face - Blue
+            -0.5f, -0.5f, -0.5f,  0f, 0f, 1f, // Blue
+            0.5f, -0.5f, -0.5f,  0f, 0f, 1f, 
+            0.5f, -0.5f,  0.5f,  0f, 0f, 1f, 
+            0.5f, -0.5f,  0.5f,  0f, 0f, 1f, 
+            -0.5f, -0.5f,  0.5f,  0f, 0f, 1f, 
+            -0.5f, -0.5f, -0.5f,  0f, 0f, 1f, 
 
-            -0.5f,  0.5f, -0.5f, 1.0f,0.0f,1.0f,
-            0.5f,  0.5f, -0.5f,  1.0f,0.0f,1.0f,
-            0.5f,  0.5f,  0.5f, 1.0f,0.0f,1.0f,
-            0.5f,  0.5f,  0.5f,  1.0f,0.0f,1.0f,
-            -0.5f,  0.5f,  0.5f, 1.0f,0.0f,1.0f,
-            -0.5f,  0.5f, -0.5f,  1.0f,0.0f,1.0f,
-        };
+            // Top face - Violet
+            -0.5f,  0.5f, -0.5f,  0.5f, 0f, 0.5f, // Violet
+            0.5f,  0.5f, -0.5f,  0.5f, 0f, 0.5f, 
+            0.5f,  0.5f,  0.5f,  0.5f, 0f, 0.5f, 
+            0.5f,  0.5f,  0.5f,  0.5f, 0f, 0.5f, 
+            -0.5f,  0.5f,  0.5f,  0.5f, 0f, 0.5f, 
+            -0.5f,  0.5f, -0.5f,  0.5f, 0f, 0.5f
+};
         /*uint[] indices = {
             0,1,3,
             1,2,3
@@ -91,6 +98,7 @@ namespace mainFile
                 Close();
             }
             spinnyvalue += 0.3f;
+            spinnybutx += 0.2f;
         }
         protected override void OnLoad()
         {
@@ -125,13 +133,14 @@ namespace mainFile
             shader.Use();
             //matrices handling for teh future but for now we're doing this color shit
             float time = (float)gettime();
-            
-            Matrix4 view = Matrix4.CreateTranslation(0.0f, 0.0f, -3.0f);
+            Matrix4 view = Matrix4.CreateTranslation(0.0f,0.0f,-3.0f);
             Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), 800f / 600f, 0.1f, 100.0f);
-            Matrix4 model = Matrix4.CreateRotationY(MathHelper.DegreesToRadians(spinnyvalue));
+            //Matrix4 model = Matrix4.CreateRotationY(MathHelper.DegreesToRadians(spinnyvalue));
+            //model = Matrix4.CreateRotationX(MathHelper.DegreesToRadians(spinnybutx));
             int modelLoc = GL.GetUniformLocation(shader.Handle, "model");
             int viewLoc = GL.GetUniformLocation(shader.Handle, "view");
             int projectionLoc = GL.GetUniformLocation(shader.Handle, "proj");
+            Matrix4 model = Matrix4.CreateRotationY(MathHelper.DegreesToRadians(spinnyvalue)) * Matrix4.CreateRotationX(MathHelper.DegreesToRadians(spinnybutx));
             GL.UniformMatrix4(modelLoc, false, ref model);
             GL.UniformMatrix4(viewLoc, false, ref view);
             GL.UniformMatrix4(projectionLoc, false, ref projection);
